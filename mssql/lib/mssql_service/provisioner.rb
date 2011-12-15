80055729169a984c94bd86e092589270a6b1cb8a
viovanov <vlad.iovanov@uhurusoftware.com>
2011-12-15 09:30:41 +0200

    Changed file headers

--- mssql/lib/mssql_service/provisioner.rb
+++ mssql/lib/mssql_service/provisioner.rb
@@ -1,4 +1,5 @@
 # Copyright (c) 2009-2011 VMware, Inc.
+# Copyright (c) 2011 Uhuru Software, Inc., All Rights Reserved
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..', 'base', 'lib')
 
 require 'base/provisioner'
