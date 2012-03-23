c386db0eafd7d90ba76e8e4faf6e6a32207ad268
stefans <stefan.schneider@uhurusoftware.com>
2012-03-23 13:33:36 +0200

    updated mssql gateway

--- mssql/lib/mssql_service/provisioner.rb
+++ mssql/lib/mssql_service/provisioner.rb
@@ -1,16 +1,10 @@
 # Copyright (c) 2009-2011 VMware, Inc.
 # Copyright (c) 2011 Uhuru Software, Inc., All Rights Reserved
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..', 'base', 'lib')
 
-require 'base/provisioner'
 require 'mssql_service/common'
 
 class VCAP::Services::Mssql::Provisioner < VCAP::Services::Base::Provisioner
 
   include VCAP::Services::Mssql::Common
-
-  def node_score(node)
-    node['available_storage'] if node
-  end
 
 end
