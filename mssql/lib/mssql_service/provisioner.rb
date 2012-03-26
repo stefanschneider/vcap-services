adce1545439a837277936570c6f20dd97216b350
stefans <stefan.schneider@uhurusoftware.com>
2012-03-26 13:28:43 +0300

    changes the new mssql gateway to work with the current mssql node

--- mssql/lib/mssql_service/provisioner.rb
+++ mssql/lib/mssql_service/provisioner.rb
@@ -7,4 +7,10 @@ class VCAP::Services::Mssql::Provisioner < VCAP::Services::Base::Provisioner
 
   include VCAP::Services::Mssql::Common
 
+  # TODO: remove this overridden method after the mssql_node is changes to support available_capacity instead of available_storage
+  # left only for backwards compatible reasons with mssql_node
+  def node_score(node)
+    node['available_storage'] if node
+  end
+
 end
